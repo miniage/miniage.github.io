df26da28d7728249a43ba276cda19ae47ff96c65
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-11 18:36:59 +0000

    Create button.js

---
+++ js/components/base/button.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    <rect x="120" height="40" width="200" rx="7" stroke="#b5b5b5" fill="#f7f7f7" onClick={this.props.onClick}></rect>
+  }
+}
+
+export default Button;
