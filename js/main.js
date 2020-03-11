52e25f6d8051152333b4daeaec08370d2f11b2c0
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-11 18:38:29 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-class OS extends React.Component {
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as Updater from "/js/caching/update.js";
+
+class OS extends Component {
   render() {
     return (
       <svg width="100%" height="100%">
-        <button onClick={update}>Update MiniageOS</button>
+        <button onClick={Updater.update}>Update MiniageOS</button>
         <p>Welcome to MiniageOS</p>
         <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
       </svg>
