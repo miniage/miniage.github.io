0b58b990ea2a5abf10f07984e6ebb5a96a181b9b
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-11 08:48:24 +0000

    Update and rename js/caching/worker.js to worker.js

---
+++ worker.js
@@ -0,0 +1,33 @@
+const cacheName = "miniage";
+const toCache = ["/"]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(toCache);
+    }
+  );
+});
+  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+        
+        let cachedResponse = response.clone();
+        
+        caches.open(cacheName).then(cache => {
+          cache.put(event.request, cachedResponse);
+        }
+
+        return response;
+      });
+    }
+  )
+});
