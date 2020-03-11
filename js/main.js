f16e3ee97ee27079dfa4f17e98efe6241bfeb832
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-11 18:03:45 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 class OS extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <svg width="100%" height="100%">
         <button onClick={update}>Update MiniageOS</button>
         <p>Welcome to MiniageOS</p>
-      </React.Fragment>
+        <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+      </svg>
     );
   }
 }
