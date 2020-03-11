ebecebff9a27e2702a4280d356ab481c3cd26302
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-11 08:37:02 +0000

    Update update.js

--- js/caching/update.js
+++ js/caching/update.js
@@ -1,1 +1,9 @@
-
+function update() {
+  if ('caches' in window) {
+    window.caches.open('miniage').then(cache => {
+    });
+    window.location.reload();
+  } else {
+    alert("Your browser doesn't support updating...");
+  }
+}
