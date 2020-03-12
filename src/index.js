78b92bb25fb277588f04146560b1dfa5a1d50893
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-12 14:25:20 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class OS extends Component {
   render() {
     return (
       <svg width="100%" height="100%">
-        <Button onClick={Update}>Update MiniageOS</button>
+        <Button onClick={Update}>Update MiniageOS</Button>
         <circle cx="50" cy="50" r="40" stroke="green" strokeWidth="4" fill="yellow" />
       </svg>
     );
