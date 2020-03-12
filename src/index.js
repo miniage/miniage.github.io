c137452cf41b37d0af8cab895faf105ad5cfe859
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-12 14:18:10 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Update from "./caching/update.js";
+import Button from "./components/base/button.js";
 
 class OS extends Component {
   render() {
     return (
       <svg width="100%" height="100%">
-        <button onClick={Update}>Update MiniageOS</button>
-        <p>Welcome to MiniageOS</p>
+        <Button onClick={Update}>Update MiniageOS</button>
         <circle cx="50" cy="50" r="40" stroke="green" strokeWidth="4" fill="yellow" />
       </svg>
     );
