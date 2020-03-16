2007e02bd4d24eaeea14e3676b8fb9f53b939051
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-16 12:08:40 +0000

    Update button.js

--- src/components/base/button.js
+++ src/components/base/button.js
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 class Button extends Component {
   render() {
     return (
-      <rect x="120" height="40" width="200" rx="7" stroke="#b5b5b5" fill="#f7f7f7" onClick={this.props.onClick}></rect>
+      <defs>
+        <linearGradient id="ubuntu-button-style1" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" style="stop-color:#ffffff;stop-opacity:1" />
+          <stop offset="100%" style="stop-color:#f3f1f1;stop-opacity:1" />
+        </linearGradient>
+      </defs>
+      <rect x="120" height="40" width="200" rx="7" stroke="#737373" fill="url(#ubuntu-button-style1)" onClick={this.props.onClick}>{this.props.children}</rect>
     );
   }
 }
