1cd1d98ceeedc6ead3a4d152bae8d81feb1def4b
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-16 12:18:30 +0000

    Update button.js

--- src/components/base/button.js
+++ src/components/base/button.js
@@ -6,8 +6,8 @@ class Button extends Component {
       <React.Fragment>
         <defs>
           <linearGradient id="ubuntu-button-style1" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" style="stop-color:#ffffff;stop-opacity:1" />
-            <stop offset="100%" style="stop-color:#f3f1f1;stop-opacity:1" />
+            <stop offset="0%" style=={{stopColor: "#ffffff", stopOpacity: 1}} />
+            <stop offset="100%" style={{stopColor: "#f3f1f1", stopOpacity: 1}} />
           </linearGradient>
         </defs>
         <rect x="120" height="40" width="200" rx="7" stroke="#737373" fill="url(#ubuntu-button-style1)" onClick={this.props.onClick}>{this.props.children}</rect>
