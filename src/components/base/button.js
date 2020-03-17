b1d3d70b833ebd1bd56f16951222d2b8af05e996
Minion <34243578+Minion3665@users.noreply.github.com>
2020-03-17 09:50:06 +0000

    Update button.js

--- src/components/base/button.js
+++ src/components/base/button.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+const cursorStyle = {
+  cursor: "pointer",
+}
+
 class Button extends Component {
   constructor(props) {
     super(props);
@@ -19,16 +23,16 @@ class Button extends Component {
           </linearGradient>
         </defs>
         <g>
-          <this.rect x="0" y="0" height="40" width={this.state.width} rx="7" stroke="#737373" fill="url(#ubuntu-button-style1)" onClick={this.props.onClick}/>
-          <this.text x={this.state.width/2} y="20" fill="#4c4c4c" fontSize="25" fontFamily="Ubuntu" dominantBaseline="middle" textAnchor="middle" onClick={this.props.onClick}>{this.props.children}</this.text>
+          <this.rect style={cursorStyle} x="0" y="0" height="40" width={this.state.width} rx="7" stroke="#737373" fill="url(#ubuntu-button-style1)" onClick={this.props.onClick}/>
+          <this.text style={cursorStyle} x={this.state.width/2} y="20" fill="#4c4c4c" fontSize="25" fontFamily="Ubuntu" dominantBaseline="middle" textAnchor="middle" onClick={this.props.onClick}>{this.props.children}</this.text>
         </g>
       </React.Fragment>
     );
   }
   componentDidMount() {
-    this.setState(
+    this.setState({
       width: this.text.width + 10,
-    );
+    });
   }
 }
 
